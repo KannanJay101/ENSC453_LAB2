5bbd1a31d21f336989b3354bf800d62e05f40862
kja56 <kja56@fas-rla-08.fas.sfu.ca>
2026-01-26 13:50:55 -0800

    Added some comment notes for better understanding

--- lab1-base-code/mm.cpp
+++ lab1-base-code/mm.cpp
@@ -1,117 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<omp.h>
+#include <omp.h>
 #include "my_timer.h"
 
 #define NI 2048
 #define NJ 2048
 #define NK 2048
 
-
-
 /* Array initialization. */
-static
-void init_array(float C[NI*NJ], float A[NI*NK], float B[NK*NJ])
+static void init_array(float C[NI * NJ], float A[NI * NK], float B[NK * NJ])
 {
   int i, j;
 
   for (i = 0; i < NI; i++)
     for (j = 0; j < NJ; j++)
-      C[i*NJ+j] = (float)((i*j+1) % NI) / NI;
+      C[i * NJ + j] = (float)((i * j + 1) % NI) / NI;
   for (i = 0; i < NI; i++)
     for (j = 0; j < NK; j++)
-      A[i*NK+j] = (float)(i*(j+1) % NK) / NK;
+      A[i * NK + j] = (float)(i * (j + 1) % NK) / NK;
   for (i = 0; i < NK; i++)
     for (j = 0; j < NJ; j++)
-      B[i*NJ+j] = (float)(i*(j+2) % NJ) / NJ;
+      B[i * NJ + j] = (float)(i * (j + 2) % NJ) / NJ;
 }
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
 static
 
-void print_array(float C[NI*NJ])
+    void
+    print_array(float C[NI * NJ])
 {
   int i, j;
 
   for (i = 0; i < NI; i++)
     for (j = 0; j < NJ; j++)
-      printf("C[%d][%d] = %f\n", i, j, C[i*NJ+j]);
+      printf("C[%d][%d] = %f\n", i, j, C[i * NJ + j]);
 }
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static
-void print_array_sum(float C[NI*NJ])
+static void print_array_sum(float C[NI * NJ])
 {
   int i, j;
 
   float sum = 0.0;
-  
+
   for (i = 0; i < NI; i++)
     for (j = 0; j < NJ; j++)
-      sum += C[i*NJ+j];
+      sum += C[i * NJ + j];
 
   printf("sum of C array = %f\n", sum);
 }
 
+//* Note: This CPU contains -> 6 P-Cores (Fast) , 8 E-Cores (Slower),  6 P-Cores + 8 E-Cores = 20 Threads in Total
 
-/* Main computational kernel. The whole function will be timed,
-   including the call and return. */
-static
-void kernel_gemm(float C[NI*NJ], float A[NI*NK], float B[NK*NJ], float alpha, float beta)
+#define Block_Size  = 32 //* making block size 32, Utilzing tiling concept to use the data size that fits in the cache 
+
+//! Main computational kernel. The whole function will be timed,  including the call and return. */
+// static void kernel_gemm(float C[NI * NJ], float A[NI * NK], float B[NK * NJ], float alpha, float beta)
+// {
+//   int i, j, k;
+
+//   // => Form C := alpha*A*B + beta*C,
+//   // A is NIxNK
+//   // B is NKxNJ
+//   // C is NIxNJ
+//   // #pragma omp parallel for private(j,k)
+
+//   //* Tiling (Do mulitplication in small chunks that fit in the cache), therefore the CPU reuses data instead of doing again
+
+ 
+//   for (i = 0; i < NI; i++)
+//   {
+
+//     for (j = 0; j < NJ; j++)
+//     {
+//       C[i * NJ + j] *= beta;
+//     }
+//     for (j = 0; j < NJ; j++)
+//     {
+//       for (k = 0; k < NK; ++k)
+//       {
+//         C[i * NJ + j] += alpha * A[i * NK + k] * B[k * NJ + j];
+//       }
+//     }
+//   }
+// }
+
+//! New kernal_gemm for lab 2 
+static void kernel_gemm(float C[NI * NJ], float A[NI * NK], float B[NK * NJ], float alpha, float beta)
 {
   int i, j, k;
 
-// => Form C := alpha*A*B + beta*C,
-//A is NIxNK
-//B is NKxNJ
-//C is NIxNJ
-//#pragma omp parallel for private(j,k) //* Strategy 1: Parallelize the outermost i loop
-  for (i = 0; i < NI; i++) {
-  #pragma omp parallel for private(j) //*Strategy 2: Parallelize the j loop 
-    for (j = 0; j < NJ; j++) {
-      C[i*NJ+j] *= beta;
+  // => Form C := alpha*A*B + beta*C,
+  // A is NIxNK
+  // B is NKxNJ
+  // C is NIxNJ
+  // #pragma omp parallel for private(j,k)
+
+  //! Tiling (Do mulitplication in small chunks that fit in the cache), therefore the CPU reuses data instead of doing again
+
+ 
+  for (i = 0; i < NI; i++)
+  {
+
+    for (j = 0; j < NJ; j++)
+    {
+      C[i * NJ + j] *= beta;
     }
-  #pragma omp parallel for private(j)//*Strategy 2: Parallelize the j loop 
-    for (j = 0; j < NJ; j++) { 
-      for (k = 0; k < NK; ++k) {
-	C[i*NJ+j] += alpha * A[i*NK+k] * B[k*NJ+j];
+    for (j = 0; j < NJ; j++)
+    {
+      for (k = 0; k < NK; ++k)
+      {
+        C[i * NJ + j] += alpha * A[i * NK + k] * B[k * NJ + j];
       }
     }
   }
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
-  
   /* Variable declaration/allocation. */
-  float *A = (float *)malloc(NI*NK*sizeof(float));
-  float *B = (float *)malloc(NK*NJ*sizeof(float));
-  float *C = (float *)malloc(NI*NJ*sizeof(float));
+  float *A = (float *)malloc(NI * NK * sizeof(float));
+  float *B = (float *)malloc(NK * NJ * sizeof(float));
+  float *C = (float *)malloc(NI * NJ * sizeof(float));
 
   /* Initialize array(s). */
-  init_array (C, A, B);
+  init_array(C, A, B);
 
   /* Start timer. */
   timespec timer = tic();
 
   /* Run kernel. */
-  kernel_gemm (C, A, B, 1.5, 2.5);
+  kernel_gemm(C, A, B, 1.5, 2.5);
 
   /* Stop and print timer. */
   toc(&timer, "kernel execution");
-  
+
   /* Print results. */
-  print_array_sum (C);
+  print_array_sum(C);
   printf("OpenMP will use up to %d threads\n", omp_get_max_threads());
-  //printf("Running with %d threads\n", omp_get_max_threads());
+  // printf("Running with %d threads\n", omp_get_max_threads());
 
   /* free memory for A, B, C */
   free(A);
   free(B);
   free(C);
-  
+
   return 0;
 }
